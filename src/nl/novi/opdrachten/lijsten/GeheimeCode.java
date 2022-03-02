0e46b2a70fb9dcd41dffe9f1d28139fd9c152157
michieloosterhuis <michiel.oosterhuis@gmail.com>
2022-03-02 23:01:29 +0100

    Assignment completed

--- src/nl/novi/opdrachten/lijsten/GeheimeCode.java
+++ src/nl/novi/opdrachten/lijsten/GeheimeCode.java
@@ -30,6 +30,8 @@ public class  GeheimeCode {
         b) Als de positie in de lijst een even getal is, dan moet de cijfercombinatie omgedraaid worden.
          */
 
+        System.out.println(encryptor(laResistanceMembers));
+
         /*
         Opdracht 3:
         Schrijf een methode die de versleutelde lijst kan omzetten naar de ontsleutelde lijst.
@@ -41,5 +43,31 @@ public class  GeheimeCode {
             members.add(name);
         }
         return members;
+    }
+
+    static List<String> encryptor(List<String> laResistanceMembers) {
+        String alphabetLower = " abcdefghijklmnopqrstuvwxyz";
+        String alphabetUpper = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        List<String> encryptedNames = new ArrayList<>();
+
+        for (int i = 0; i < laResistanceMembers.size(); i++) {
+            String memberName = laResistanceMembers.get(i);
+            String[] memberNameArray = new String[laResistanceMembers.get(i).length()];
+            for (int j = 0; j < memberNameArray.length; j++) {
+                String currentCharacter = Character.toString(memberName.charAt(j));
+                if(alphabetLower.contains(currentCharacter)) {
+                    memberNameArray[j] = Integer.toString(alphabetLower.indexOf(currentCharacter));
+                } else {
+                    memberNameArray[j] = Integer.toString(alphabetUpper.indexOf(currentCharacter) + 100);
+                };
+                if (i % 2 == 0) {
+                    StringBuilder sb = new StringBuilder(memberNameArray[j]);
+                    memberNameArray[j] = sb.reverse().toString();
+                }
+            }
+            encryptedNames.add(String.join("-", memberNameArray));
+        }
+        return encryptedNames;
     }
 }
--- src/nl/novi/opdrachten/lijsten/VersleutelNamenLijst.java
+++ src/nl/novi/opdrachten/lijsten/VersleutelNamenLijst.java
@@ -1,5 +1,15 @@
 package nl.novi.opdrachten.lijsten;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static nl.novi.opdrachten.lijsten.GeheimeCode.encryptor;
+
 public class VersleutelNamenLijst {
 
     /**
@@ -10,6 +20,27 @@ public class VersleutelNamenLijst {
      *
      */
     public static void main(String[] args) {
+        try {
+            List<String> names = new ArrayList<>();
+            File file = new File("src/nl/novi/opdrachten/lijsten/verzetsleden.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                names.add(scanner.nextLine());
+            }
+            names = encryptor(names);
 
+            File newFile = new File("src/nl/novi/opdrachten/lijsten/verzetsleden_encrypted.txt");
+            FileWriter fileWriter = new FileWriter(newFile);
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+            for (String name : names) {
+                bw.write(name);
+                bw.newLine();
+            }
+            bw.close();
+            System.out.println(names);
+        } catch (IOException e) {
+            System.out.println("Inlezen bestand mislukt...");
+            System.out.println(e);
+        }
     }
 }
